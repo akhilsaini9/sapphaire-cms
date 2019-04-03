5ed5021d8350621941b249b95192e4a0b41e1268
akhilsaini9 <49193522+akhilsaini9@users.noreply.github.com>
2019-04-03 16:22:34 +0530

    configure

---
+++ src/main/java/com/sapphire/web/config/SapphireCMSWebInitializer.java
@@ -0,0 +1,14 @@
+package com.sapphire.web.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapphireCMSWebInitializer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(SapphireCMSWebInitializer.class, args);
+	}
+
+}
