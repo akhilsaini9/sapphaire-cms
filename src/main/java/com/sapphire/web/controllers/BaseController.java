ca6364ae86f1a1fe93fff61a2625f99d8de88cd7
akhilsaini9 <49193522+akhilsaini9@users.noreply.github.com>
2019-04-03 16:51:03 +0530

    BaseController Config

---
+++ src/main/java/com/sapphire/web/controllers/BaseController.java
@@ -0,0 +1,12 @@
+package com.sapphire.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BaseController {
+
+
+	
+}
